687c8e6607f2710c805a0cb121d78884659f4467
rossiaj <rossiaj@proton.me>
2024-06-28 13:54:07 -0400

    changes made from theia for shopping app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,69 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
-import { AppProvider } from './context/AppContext';
+// import { AppProvider } from './context/AppContext';
 import CartValue from './components/CartValue';
 import ExpenseList from './components/ExpenseList';
 import ItemSelected from './components/ItemSelected';
 import Location from './components/Location';
+// adding the following lines to add components not already listed
+import ExpenseItem from './components/ExpenseItem';
+// end of added code
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+// following 6 lines of code replaced by code immediately following 
+// function App() {
+//   return (
+//     <div className="App">
+//     </div>
+//   );
+// }
+import { AppProvider } from './context/AppContext';
+const App = () => {
+    return (
+        <AppProvider>
+            <div className='container'>
+                    <div className='row mt-3'>
+                        {
+                                            // CartValue component
+                    <div className='col-sm'>
+                        <CartValue />
+                    </div>
+                        }        
+
+                        {
+                                             //ExpenseList component
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
+                        }        
+        
+                                
+
+                        {
+                                             //ItemSelected component
+                    <div className='col-sm'>
+                        <ItemSelected />
+                    </div>
+                        }        
+
+                        {
+                                             //Location component
+                    <div className='col-sm'>
+                        <Location />
+                    </div>
+                        }        
+                        {
+                                             //ExpenseItem component
+                    <div className='col-sm'>
+                        <ExpenseItem />
+                    </div>
+                        }        
+
+
+                </div>
+            </div>
+        </AppProvider>
+    );
+};
+// export default App;
 
 export default App;
--- src/components/CartValue.js
+++ src/components/CartValue.js
@@ -1,7 +1,26 @@
+// following 7 lines are original code
+// import React, { useContext } from 'react';
+// import { AppContext } from '../context/AppContext';
+// 
+// const CartValue = () => {
+// };
+// 
+// export default CartValue;
+// following code copied from HOL
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const CartValue = () => {
+    const { expenses, Location } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += (item.unitprice * item.quantity));
+    }, 0);
+
+    return (
+        <div className='alert alert-primary'>
+            <span>Cart Value: {Location}{totalExpenses}</span>
+        </div>
+    );
 };
 
 export default CartValue;
